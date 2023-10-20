ca33547e0fb0b00f18e047d08f25db534ffb5323
Miahri <mahrihommadova@gmail.com>
2023-10-20 21:44:18 +0300

    added 4th homework for Monday level

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -23,14 +23,14 @@ export const HW3 = () => {
   const handleSave = () => {
     // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
     // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
-    setTexts([...texts, currentText]);
+    setTexts([currentText, ...texts]);
     setCurrentText('');
   };
 
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
